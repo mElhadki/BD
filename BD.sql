2a584a069fb7e52cad64b539759900b21f9cfc8d
mElhadki <elhadkimariem3@gmail.com>
2020-04-28 17:05:14 +0000

    New Modification

--- BD.sql
+++ BD.sql
@@ -4,7 +4,6 @@ Create database GRecette ;
 use GRecette;
 
 
-
 /*==============================================================*/
 /* Table: Ingredient                                            */
 /*==============================================================*/
@@ -13,8 +12,7 @@ create table Ingredient (
    Nom_Ingredient       varchar(254)         null,
    Calories_Ingredient  varchar(254)         null,
    constraint PK_INGREDIENT primary key nonclustered (Ref_Ingredient)
-)
-go
+);
 
 /*==============================================================*/
 /* Table: Produit                                               */
@@ -27,8 +25,7 @@ create table Produit (
    Quantite_Produit     int                  null,
    Prix_Produit         float                null,
    constraint PK_PRODUIT primary key nonclustered (Ref_Produit)
-)
-go
+);
 
 /*==============================================================*/
 /* Table: Rangement                                             */
@@ -37,8 +34,8 @@ create table Rangement (
    Ref_Rangement        int                  not null,
    Nom_Rangement        varchar(254)         null,
    constraint PK_RANGEMENT primary key nonclustered (Ref_Rangement)
-)
-go
+);
+
 
 /*==============================================================*/
 /* Table: Recette                                               */
@@ -51,8 +48,7 @@ create table Recette (
    Niveau_Difficulte    varchar(254)         null,
    NbrPersonne          int                  null,
    constraint PK_RECETTE primary key nonclustered (Ref_Recette)
-)
-go
+);
 
 /*==============================================================*/
 /* Index: ASSOCIATION_1_FK                                      */
@@ -60,15 +56,14 @@ go
 create index ASSOCIATION_1_FK on Produit (
 Ref_Rangement ASC
 )
-go
+;
 
 /*==============================================================*/
 /* Index: ASSOCIATION_2_FK                                      */
 /*==============================================================*/
 create index ASSOCIATION_2_FK on Produit (
 Ref_Ingredient ASC
-)
-go
+);
 
 /*==============================================================*/
 /* Table: Association_3                                         */
@@ -77,8 +72,7 @@ create table Association_3 (
    Ref_Ingredient       int                  not null,
    Ref_Recette          int                  not null,
    constraint PK_ASSOCIATION_3 primary key (Ref_Ingredient, Ref_Recette)
-)
-go
+);
 
 
 /*==============================================================*/
@@ -86,98 +80,81 @@ go
 /*==============================================================*/
 create index ASSOCIATION_3_FK on Association_3 (
 Ref_Ingredient ASC
-)
-go
+);
 
 /*==============================================================*/
 /* Index: ASSOCIATION_3_FK2                                     */
 /*==============================================================*/
 create index ASSOCIATION_3_FK2 on Association_3 (
 Ref_Recette ASC
-)
-go
+);
 
 
 
 alter table Association_3
    add constraint FK_ASSOCIAT_ASSOCIATI_INGREDIE foreign key (Ref_Ingredient)
       references Ingredient (Ref_Ingredient)
-go
+;
 
 alter table Association_3
    add constraint FK_ASSOCIAT_ASSOCIATI_RECETTE foreign key (Ref_Recette)
       references Recette (Ref_Recette)
-go
+;
 
 alter table Produit
    add constraint FK_PRODUIT_ASSOCIATI_RANGEMEN foreign key (Ref_Rangement)
       references Rangement (Ref_Rangement)
-go
+;
 
 alter table Produit
    add constraint FK_PRODUIT_ASSOCIATI_INGREDIE foreign key (Ref_Ingredient)
       references Ingredient (Ref_Ingredient)
-go
+;
 
 
 /* Creation des utilisateurs */
+  
+CREATE USER 'Mariem'@'localhost' IDENTIFIED BY 'Admin123';
+GRANT ALL PRIVILEGES ON * . * TO 'Mariem'@'localhost';
+FLUSH PRIVILEGES;  
+;
 
 
--- Creates the login Mariem with password 'Admin123'.  
-CREATE LOGIN Mariem   
-    WITH PASSWORD = 'Admin123';  
-GO  
+CREATE USER 'user'@'localhost' IDENTIFIED BY '123456';
+GRANT CREATE, SELECT ON * . * TO 'user'@'localhost';
 
--- Creates a database user for the login created above.  
-CREATE USER Mariem FOR LOGIN Mariem;  
-GO  
- 
-GRANT all TO Mariem;  
-GO 
-
--- Creates the login Utilisateur with password 'Utilisateur123'.  
-CREATE LOGIN Utilisateur   
-    WITH PASSWORD = 'Utilisateur123';  
-GO  
-
--- Creates a database user for the login created above.  
-CREATE USER Utilisateur FOR LOGIN Utilisateur;  
-GO  
- 
-GRANT select,insert to Utilisateur ;  
-GO 
 
 -- Insertion --
 
 -- Insert Into Table Recette --
 
-insert into Recette values (1,'Artichauts poivrade','des petits artichauts violets','25500 Kcal','Facile',4) 
-insert into Recette values (2,'Avocat au thon','avocat, thon, herbes hachées et mayonnaise','7000 Kcal','Moyenne',2) 
-insert into Recette values (3,'Coleslaw Breton','salade de chou et carotte','37500 Kcal','Facile',5) 
+insert into Recette values (1,'Artichauts poivrade','des petits artichauts violets','25500 Kcal','Facile',4);
+insert into Recette values (2,'Avocat au thon','avocat, thon, herbes hachées et mayonnaise','7000 Kcal','Moyenne',2); 
+insert into Recette values (3,'Coleslaw Breton','salade de chou et carotte','37500 Kcal','Facile',5);
 
 -- Insert Into Table Rangement --
 
-insert into Rangement values (1,'Rangement1')
+insert into Rangement values (1,'Rangement1');
 
 -- Insert Into Table Ingredient --
 
-insert into Ingredient values (1,'artichauts violets','2000 Lcal')
+insert into Ingredient values (1,'artichauts violets','2000 Lcal');
 
 -- Insert Into Table Produit --
 
-insert into Produit values (1,1,1,'salade avec des herbes',2,200.49)
+insert into Produit values (1,1,1,'salade avec des herbes',2,200.49);
 
 -- Insert Into Table Association --
 
-insert into Association_3 values (1,1)
-insert into Association_3 values (1,2)
+insert into Association_3 values (1,1);
+insert into Association_3 values (1,2);
 
 -- Select From All tables --
 
-select * from Recette
-select * from Rangement
-select * from Ingredient
-select * from Produit
+select * from Recette;
+select * from Rangement;
+select * from Ingredient;
+select * from Produit;
 
 -- Update -- 
 
